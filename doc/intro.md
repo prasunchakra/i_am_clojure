105ebe7f34cb3778da86d2134fceb04f83fd1402
Prasun Chakraborty <lordprasun@gmail.com>
2020-05-31 09:09:51 +0530

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to i_am_clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/i_am_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns i-am-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [i-am-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
